2d0a5698fe7e47ff8a3bb446e32365622c0f8e34
wftheron <3618269@myuwc.ac.za>
2019-01-30 08:40:49 +0200

    first commit

---
+++ DAY1_Laminaria.R
@@ -0,0 +1,86 @@
+#Day 1
+#Laminaria dataset_exploring and learning
+#Wilmari Theron
+#29 jan 2019
+
+#loading libraries
+library(tidyverse)
+lam <- read_csv("data/laminaria.csv")
+head(lam) #shows the first six rows
+tail(lam) #shows the last six rows
+head(lam, n = 3)
+lam_select <- lam %>%
+  select(site, total_length) %>% 
+  slice(54:80)
+view(lam)
+lam_kom <- lam %>% 
+  filter(site == "Kommetjie")
+view(lam_kom)
+#in the laminaria dataset, select only site and blade length column
+#filter only for seapoint
+lam_try <- lam %>% 
+  select(site, blade_length) %>% 
+  filter(site == "Sea Point")
+view(lam_try)
+
+lam %>% 
+  filter(total_length == max(total_length))
+
+summary(lam)
+
+lam %>% 
+  summarise(avrg_bl = mean(blade_length),
+            med_bl = median(blade_length),
+            sd_bl = sd(blade_length))
+
+#now for the standard error
+#select lam data and group by site,
+#then summarise blade length of those sites
+lam %>% 
+  group_by(site) %>% 
+  summarise(var_bl = var(blade_length),
+            n = n()) %>% 
+  mutate(se = sqrt(var_bl/n))
+
+#remove columnns?
+lam_2 <- lam %>% 
+  select(-blade_thickness, -blade_length)
+
+#select lam dataset and then select only stipe mass, then tell me how many entries you have for stipe mass
+
+lam %>% 
+  select(blade_length) %>% 
+  na.omit %>% 
+  summarise(n = n())
+lam %>% 
+  select(blade_length) %>% 
+  summarise(n = n())
+#na omit so hat your stats arent based on absense of data
+
+#make plots and graphs
+ggplot(data = lam, aes(x = stipe_mass, y = stipe_length)) + 
+  geom_point(shape = 21, colour = "orange", fill = "green") + 
+  labs(x = "Stipe mass (kg)", y = "stipe length (cm)")
+
+# Exercise: 
+
+# 1. Create a new data frame from the `laminaria` dataset that meets the following criteria: contains 
+#only the `site` column and a new column called `total_length_half` containing values that are half of the `total_length`. In this `total_length_half` column, there are no `NA`s and all values are less than 100.
+# **Hint**: think about how the commands should be ordered to produce this data frame!
+lam_new <- lam %>% 
+  select(site) %>% 
+  na.omit %>% 
+#   2. Use `group_by()` and `summarize()` to find the mean, min, and max blade_length for each site. Also add the number of observations (hint: see `?n`).
+lam %>% 
+  group_by(site) %>% 
+  summarise(avrg_bl = mean(blade_length),
+            min_bl = min(blade_length),
+            max_bl = max(blade_length),
+            n = n())
+
+#. What was the heaviest stipe measured in each site? Return the columns `site`, 
+#`region`, and `stipe_length`.
+lam_new1 <- lam %>% 
+  group_by(site, region, stipe_length) %>% 
+  summarise(heaviest_stipe = max(stipe_mass))
+            
---
+++ day1.R
@@ -0,0 +1,26 @@
+#Day1
+#Simple Stats
+mass <- 48
+age * 126
+age - 17
+mass_index <- mass / age# What are the values after each statement in the following?
+  
+mass <- 48              # mass?
+mass <- mass * 2.0      # mass?
+age  <- 126             # age?
+age  <- age - 17        # age?
+mass_index <- mass/age  # mass_index?
+
+Use R to calculate some simple mathematical expressions entered.
+x <- 40
+
+y <- 23
+z <- (x - y)
+
+Assign the value of 40 to x
+Assign the value of 23 to y
+Make z the value of x-y
+Display z in the consol
+
+#EXERCISE
+#Calculate the monthly climatology per site
