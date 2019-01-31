3e16f57dc3c5fca3bdaf26be0409b66da5676c39
wftheron <3618269@myuwc.ac.za>
2019-01-31 08:35:26 +0200

    day 2:exercise 1

--- EXERCISE 1.R
+++ EXERCISE 1.R
@@ -5,18 +5,49 @@
 # #remember: R = Plotting with a purpose, have hypothesis
 
 library(tidyverse)
+
 #CARS
 #assigning and getting background info
 car <- datasets::cars
 ??cars
-ggplot(car, aes( x = speed, y = dist)) +
+#Hypothesis:Does the Stopping Distance (fft) increase with the cars' Speed (mph)
+xplot_1 <- ggplot(car, aes( x = speed, y = dist)) +
   geom_point(aes(colour = dist)) +
   geom_smooth(method = "lm") +
   labs( x = "Speed (mph)", y = "Stopping Distance (fft)") +
   ggtitle("Speed and Stopping Distances of Cars") 
 
+xplot_1    #view plot
 
-#PHONES
-phones <- datasets::WorldPhones
-??phones
-ggplot(phones, aes( x = phones, y = ))
+#FLOWERS
+#assigning and getting background info
+Iris<- datasets::iris
+#?? for help function
+??iris
+#Which species of Iris has the longests Sepal Lengths?
+ 
+xplot_2 <- ggplot(Iris, aes( x = Sepal.Length, y = Species)) +
+  geom_point(aes(colour = Species)) +
+  labs( x = "Sepal Length", y = "Species)") +
+  ggtitle("Sepal Lengths of different Iris Species") 
+xplot_2
+
+#Which of the Iris species has the longest Sepal Length and Width in cm")
+xplot_3 <- ggplot(iris, aes( x = Sepal.Length, y = Sepal.Width, colour = Species)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs( x = "Sepal Length (cm)", y = "Sepal Width (cm)" ) +
+  ggtitle("Sepal Length and Width in cm for different Iris Species")
+
+#viewplot  
+xplot_3
+
+# average waiting time  between erruption in relation to the duration of the erruption of the Old Faithful in YellowstonePark
+faith <- datasets::faithful
+??faithful  
+xplot_4 <- ggplot(faith, aes( x = waiting, y = eruptions) +
+                    geom_point()
+  
+  
+  xplot_4#Laminaria
+lam <- read_csv("data/laminaria.csv")
