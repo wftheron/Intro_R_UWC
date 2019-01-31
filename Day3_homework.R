5939713ffbc72249beb5658f571093ba133b30f2
wftheron <3618269@myuwc.ac.za>
2019-01-31 14:55:10 +0200

    last dday3

--- Day3_homework.R
+++ Day3_homework.R
@@ -1,3 +1,16 @@
 #convert .csv to .r data
-#write 5 lines discussing the library ggsn and the library scale
-##know how to read code.
+library(tidyverse)
+library(scales)
+library(ggsn)
+
+#EXERCISE; 
+lam <- read_csv("data/laminaria.csv")
+
+laminaria <- read_csv("data/laminaria.csv")
+
+
+# write 5 lines discussing the library ggsn and the library scale
+# #There is no default scale bar function in the
+# tidyverse, which is why we have loaded the ggsn package. This package is devoted to adding scale bars
+# and North arrows to ggplot2 figures.
+# ##know how to read code.
