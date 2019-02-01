1300e6326d85088a3db293afcba1be03cf7ffd98
wftheron <3618269@myuwc.ac.za>
2019-02-01 10:24:05 +0200

    day3_final homework

--- Day3_homework.R
+++ Day3_homework.R
@@ -9,10 +9,16 @@ library(ggsn)
 laminaria <- read_csv("data/laminaria.csv")  #read csv
 save(laminaria, file = "data/laminaria.RData")   #convert csv to R
 
+#2 Discuss the library 'ggsn' and the library 'scale'
 
-
-# write 5 lines discussing the library ggsn and the library scale
-# #There is no default scale bar function in the
-# tidyverse, which is why we have loaded the ggsn package. This package is devoted to adding scale bars
-# and North arrows to ggplot2 figures.
-# ##know how to read code.
+# There is no default scale bar function in the tidyverse package, which is 
+# why we have loaded the ggsn package - This is devoted to adding scale bars in kilometres
+# to maps in geographic or metric coordinates created with "ggplot2" or "ggmap"
+# Included arguments:UNDEFINED DATA
+#   x.min; number with min x coordinate
+#   x.max; number with max X coordinate
+#   (both useful for "ggmap")
+#   y.min;number with min Y coordinate
+#   y.max;number with max Y coordinate
+  
+#3 know how to read code
