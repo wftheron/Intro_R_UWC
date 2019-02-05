e15beceb095975e2544e99aa8f1adea1853766dd
wftheron <3618269@myuwc.ac.za>
2019-02-05 13:34:36 +0200

    section4

--- SECTION 4.R
+++ SECTION 4.R
@@ -13,36 +13,44 @@ library(ggplot2)
 # assigning and getting background info
 car <- datasets::cars
 ??cars
+car %>% summarise(mean_s = mean(speed),
+                var_s = var(speed, na.rm = TRUE), # Calculate variance
+                min_sr = min(speed, na.rm = TRUE),
+                max_s = max(speed, na.rm = TRUE),
+                med_s = median(speed, na.rm = TRUE),
+                mean_di = mean(dist),
+                var_di = var(dist, na.rm = TRUE), # Calculate variance
+                min_di = min(dist, na.rm = TRUE),
+                max_di = max(dist, na.rm = TRUE),
+                med_di = median(dist, na.rm = TRUE))
 
 plot_1 <- ggplot(car, aes( x = speed, y = dist)) +
   geom_point(aes(colour = speed)) +
   geom_bar(stat="identity",aes(fill = dist), position = "dodge") +
   labs( x = "Speed (mph)", y = "Stopping Distance (fft)") +
   ggtitle("Stopping Distance of Cars in relation to their Speed") 
+plot_1 #test plot
 
+plot_2 <- ggplot(car, aes( x = speed, y = dist)) +
+   geom_jitter(color= "green") +
+  labs( x = "Speed (mph)", y = "Stopping Distance (fft)") +
+  ggtitle("Stopping Distance of Cars in relation to their Speed") 
+plot_2         #test plot; a lot like a regular geom_point
 
+plot_3 <- ggplot(car, aes( x = speed, y = dist)) +
+  geom_density2d(color= "dark green") +
+  labs( x = "Speed (mph)", y = "Stopping Distance (fft)") +
+  ggtitle("Stopping Distance of Cars in relation to their Speed")
+plot_3
 
-
-#FLOWERS
-#assigning and getting background info
-Iris<- datasets::iris
-#?? = Help/info
-??iris
-#Which species of Iris has the longests Sepal Lengths?
-# not box= non-numeric argument
-plot_2 <- ggplot(Iris, aes( x = Sepal.Length, y = Species)) +
-  geom_point(aes(colour = Species)) +
-  geom_boxplot()
-  labs( x = "Sepal Length", y = "Species)") +
-  ggtitle("Sepal Lengths of different Iris Species") 
-  
-
-
-
-
-#Which of the Iris species has the longest Sepal Length and Width in cm")
-xplot_3 <- ggplot(iris, aes( x = Sepal.Length, y = Sepal.Width, colour = Species)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  labs( x = "Sepal Length (cm)", y = "Sepal Width (cm)" ) +
-  ggtitle("Sepal Length and Width in cm for different Iris Species")
+ora <- datasets::Orange
+??Orange #The Orange data frame has 35 rows and 3 columns of records of the growth of orange trees
+# Tree = an ordered factor indicating the tree on which the measurement is made.
+ # age = days since 1968/12/31
+ # circumference = mm;
+map_O <- ggplot(ora, aes(x = age, y = circumference, color = Tree)) +
+  geom_jitter() +
+  geom_line() +
+  labs( x = "Age", y = "Circumference (mm)") +
+  ggtitle("Records of Orange trees growth") 
+map_O    #test plot
